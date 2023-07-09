027c18c69b4166a0bcb99f678b9d88ca789da732
ronyumbu <inidiaakunrony2021@gmail.com>
2023-07-09 17:52:15 +0800

    implementation of mandatory and optional criteria version 2

--- src/services/songs/SongService.js
+++ src/services/songs/SongService.js
@@ -27,7 +27,7 @@ class SongService {
 
         const query = {
             text: 'INSERT INTO songs VALUES($1, $2, $3, $4, $5, $6, $7, $8, $9) RETURNING id',
-            values: [id, title, year, performer, genre, duration, albumId, createdAt, createdAt]
+            values: [id, title, year, performer, genre, duration, albumId, createdAt]
         };
         const {rows} = await this._pool.query(query);
 
